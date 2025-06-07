83fa890193822dcc0f33ca92ad391beed763a4e0
Tash Broadwater <170302181+TBroadwater87@users.noreply.github.com>
2025-06-06 20:42:37 -0600

    Align public header and docs

--- include/ByteLite/ByteLiteSysDetect.h
+++ include/ByteLite/ByteLiteSysDetect.h
@@ -1,34 +1,6 @@
 #ifndef BYTELITESYSDETECT_H
 #define BYTELITESYSDETECT_H
 
-#include <cstdint>
-#include <string>
-
-namespace ByteLiteSysDetect {
-
-struct SystemProfile {
-    std::string cpuName;
-    int physicalCores;
-    int logicalThreads;
-    std::uint64_t ramMB;
-    std::string osName;
-    std::string osVersion;
-    bool hasMMX;
-    bool hasSSE;
-    bool hasAVX;
-    bool hasAVX2;
-    bool hasAVX512;
-    std::string fastestDrive;
-    double driveWriteMBps;
-    std::string gpuName;
-    bool is64bit;
-};
-
-SystemProfile DetectSystem();
-
-void RunSystemDetectionTest();
-
-} // namespace ByteLiteSysDetect
+#include "ByteLiteSysDetect.h"
 
 #endif // BYTELITESYSDETECT_H
-
